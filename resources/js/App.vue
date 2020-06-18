6c8c6d5d88a00c43306a384084cb4ebd5f14656e
Hassan Nawaz <hassancent@gmail.com>
2020-06-18 22:31:49 +0500

    moved styles from app to layout

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -5,9 +5,7 @@
 </template>
 
 <style>
-@import "https://fonts.googleapis.com/css?family=Nunito:400,600|Open+Sans:400,600,700";
-@import "https://use.fontawesome.com/releases/v5.4.1/css/all.css";
-@import "/css/spur.css";
+
 </style>
 
 <script>
