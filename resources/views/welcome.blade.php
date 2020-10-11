4477ea29cc6695c70088f7e0fe916b91392a7ea2
Hassan Nawaz <hassancent@gmail.com>
2020-10-11 19:30:50 +0500

    title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{env('APP_NAME')}}</title>
+        <title>NCEAC Portal</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
