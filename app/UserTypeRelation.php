cf5a50ba39c3158f4144bde0bb44fef41d4c9be8
Hassan Nawaz <hassancent@gmail.com>
2021-01-17 18:49:32 +0500

    dashboard stats

--- app/UserTypeRelation.php
+++ app/UserTypeRelation.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class UserTypeRelation extends Model
 {
     use SoftDeletes;
+
+    public function UserType() {
+        return $this->hasOne("App\UserType", "id", "user_type_id");
+    }
 }
