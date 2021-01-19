298b0308c4376bbd1b08e419f212a22c9bfabced
Bilal Khalid <bilalkhalid1000@gmail.com>
2021-01-19 21:52:22 +0500

    Removed soft delete from course. Added import courses from previous session in manage course.

--- app/Course.php
+++ app/Course.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
-    use SoftDeletes;
+    // use SoftDeletes;
 
     public function tai() {
         return $this->belongsToMany('App\User', 'tai_courses', 'course_id', 'tai_id');
